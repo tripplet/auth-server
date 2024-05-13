d8107f2b56d21190ea74c00f47c0f9d924bcd7b2
Tobias Tangemann <tobias@tangemann.org>
2024-05-13 18:37:00 +0200

    Refactor args into separate file

--- src/http.rs
+++ src/http.rs
@@ -4,7 +4,7 @@ use crate::listen;
 use crate::Config;
 
 use std::sync::{atomic::AtomicBool, atomic::Ordering, Arc};
-use std::{error::Error, fmt, fs, process};
+use std::{error::Error, process};
 
 use actix_web::cookie::{Cookie, SameSite};
 use actix_web::middleware::{Condition, Logger};
@@ -69,7 +69,7 @@ async fn generate(
 }
 
 pub async fn run_server(cfg: &Config) {
-    let secret_key = match load_secret_key(cfg) {
+    let secret_key = match cfg.get_secret() {
         Err(err) => {
             error!("{}", err);
             process::exit(-1);
@@ -134,47 +134,6 @@ pub async fn run_server(cfg: &Config) {
     }
 }
 
-#[derive(Debug)]
-enum KeyError {
-    NoKeyFound,
-    KeyToShort,
-    UnableToReadKeyFile(std::io::Error),
-}
-
-impl fmt::Display for KeyError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self {
-            Self::NoKeyFound => write!(f, "No secret defined"),
-            Self::KeyToShort => write!(
-                f,
-                "The secret key is too short and should be at least 16 characters long"
-            ),
-            Self::UnableToReadKeyFile(err) => write!(f, "Unable to read secret file: {err}"),
-        }
-    }
-}
-
-impl Error for KeyError {}
-
-fn load_secret_key(cfg: &Config) -> Result<String, KeyError> {
-    let secret_key = if let Some(secret) = &cfg.secret {
-        Ok(secret.clone())
-    } else if let Some(secret_file) = &cfg.secret_file {
-        match fs::read_to_string(secret_file) {
-            Ok(secret) => Ok(secret),
-            Err(err) => Err(KeyError::UnableToReadKeyFile(err)),
-        }
-    } else {
-        Err(KeyError::NoKeyFound)
-    };
-
-    // Basic sanity check
-    if secret_key.as_ref().is_ok_and(|k| k.len() < 16) {
-        return Err(KeyError::KeyToShort);
-    }
-
-    secret_key
-}
 
 /// Generate a cookie with the given authorization
 fn generate_cookie(
@@ -236,149 +195,5 @@ pub fn socket_from_systemd_activation(
         unsafe { Ok(Some(UnixListener::from_raw_fd(fds.remove(0).into_raw_fd()))) }
     } else {
         Err("Not supported".into())
-    }
-}
-
-#[cfg(feature = "systemd_socket_activation")]
-fn handle_systemd_activation() {
-    use std::{future::Future, pin::Pin};
-
-    let incoming = match socket_from_systemd_activation() {
-        Ok(Some(socket)) => socket,
-        Ok(None) => {
-            error!("No systemd socket activation provided");
-            process::exit(-2);
-        }
-        Err(err) => {
-            error!("Error determining socket activation: {err}");
-            process::exit(-3);
-        }
-    };
-
-    let mut tasks: Vec<Pin<Box<dyn Future<Output = Result<(), &str>>>>> = vec![];
-
-    // Add the http server to the task list
-    tasks.push(Box::pin(async move {
-        _ = server.listen_uds(incoming).unwrap().run().await;
-        Ok(())
-    }));
-
-    // Add the Ctrl+C handler to the task list
-    tasks.push(Box::pin(async move {
-        _ = signal::ctrl_c().await;
-        Ok(())
-    }));
-
-    // If a idle timeout is set, create a new timeout task and add it to the task list
-    if let Some(idle_time) = cfg.systemd_activation_idle {
-        if idle_time > 0 {
-            let timeout = tokio::time::Duration::from_secs(idle_time as u64);
-            let request_received_check = request_received.clone();
-
-            let idle_timeout = tokio::task::spawn(async move {
-                loop {
-                    tokio::time::sleep(timeout).await;
-
-                    // If the request_received has not been set to true in the timeout period => exit
-                    if request_received_check
-                        .compare_exchange(true, false, Ordering::Relaxed, Ordering::Relaxed)
-                        .is_err()
-                    {
-                        break;
-                    }
-                }
-            });
-
-            tasks.push(Box::pin(async move {
-                _ = idle_timeout.await;
-                Err("Idle timeout")
-            }));
-        }
-    }
-
-    select! {
-        result = futures::future::select_all(tasks) =>
-            match result {
-                (Err(err), ..) => error!("Closing server: {err}"),
-                _ => (),
-            }
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use std::io::Write;
-
-    use super::*;
-
-    impl Default for Config {
-        fn default() -> Self {
-            Self {
-                listen: "0.0.0.0:1234".parse().unwrap(),
-                systemd_activation_idle: Default::default(),
-                socket_group: Default::default(),
-                secret: Default::default(),
-                secret_file: Default::default(),
-                cookie_name: Default::default(),
-                verbose: Default::default(),
-            }
-        }
-    }
-
-    impl PartialEq for KeyError {
-        fn eq(&self, other: &Self) -> bool {
-            match (self, other) {
-                (Self::UnableToReadKeyFile(_), Self::UnableToReadKeyFile(_)) => true,
-                (Self::KeyToShort, Self::KeyToShort) => true,
-                (Self::NoKeyFound, Self::NoKeyFound) => true,
-                _ => false,
-            }
-        }
-    }
-
-    #[test]
-    fn test_key_to_short_cli() {
-        let cfg = Config {
-            secret: Some("test".into()),
-            ..Default::default()
-        };
-
-        assert_eq!(load_secret_key(&cfg), Err(KeyError::KeyToShort));
-    }
-
-    #[test]
-    fn test_key_to_short_file() {
-        let mut tempfile = tempfile::NamedTempFile::new().unwrap();
-        write!(tempfile, "secret123").unwrap();
-
-        let cfg = Config {
-            secret_file: Some(tempfile.path().to_str().unwrap().into()),
-            ..Default::default()
-        };
-
-        assert_eq!(load_secret_key(&cfg), Err(KeyError::KeyToShort));
-    }
-
-    #[test]
-    fn test_key_cli() {
-        let cfg = Config {
-            secret: Some("1234567890123456".into()),
-            ..Default::default()
-        };
-
-        assert_eq!(load_secret_key(&cfg), Ok("1234567890123456".into()));
-    }
-
-    #[test]
-    fn test_key_from_file() {
-        let mut tempfile = tempfile::NamedTempFile::new().unwrap();
-        write!(tempfile, "1234567890123456").unwrap();
-
-        let cfg = Config {
-            secret_file: Some(tempfile.path().to_str().unwrap().into()),
-            ..Default::default()
-        };
-
-        assert_eq!(load_secret_key(&cfg), Ok("1234567890123456".into()));
     }
 }
